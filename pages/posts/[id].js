f25d44374297c1ea0b073eb5c328233e282959d4
Uyen Do TMRW-VN <99628467+uyendo-tmrw-vn@users.noreply.github.com>
2022-12-21 02:12:11 +0700

    test for detail

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -2,47 +2,49 @@ import Layout from '../../components/layout';
 import { getAllPostIds, getPostData } from '../../lib/posts';
 
 
+
+export async function getStaticPaths() {
+    const data = await fetch('https://jsonplaceholder.typicode.com/posts');
+    const posts = await data.json();
+    console.log({ posts });
+    const Result = posts.map(post => ({ params: { id: post.id.toString() } }))
+
+    // const paths = posts.map(post =>({
+    //     params: {id: post.show.id},
+    // }))
+
+    console.log({ Result });
+    return {
+        paths: Result,
+        fallback: 'blocking'
+    }
+}
+export async function getStaticProps({ params }) {
+    console.log({ params });
+    const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
+    const post = await data.json();
+
+    return {
+        props: {
+            post,
+        },
+        revalidate: 10, // In seconds
+    }
+}
+
+
 export default function Post({ post }) {
+    console.log({ post });
     return (
         <Layout>
-            {post?.title}
+            Post : {post?.id}
             <br />
-            {post?.id}
+            {post?.title}
             <br />
             {post?.date}
         </Layout>
     );
 }
-
-// export async function getStaticPaths() {
-//     const data = await fetch('https://jsonplaceholder.typicode.com/posts');
-//     const posts = await data.json();
-//     console.log({ posts });
-//     const Result = posts.map(post => ({ params: { id: post.id.toString() } }))
-
-//     // const paths = posts.map(post =>({
-//     //     params: {id: post.show.id},
-//     // }))
-
-//     console.log({ Result });
-//     return {
-//         paths: Result,
-//         fallback: 'blocking'
-//     }
-// }
-// export async function getStaticProps({ params }) {
-//     console.log({ params });
-//     const data = await fetch(`https://jsonplaceholder.typicode.com/posts/${params.id}`);
-//     const post = await data.json();
-
-//     return {
-//         props: {
-//             post,
-//         },
-//         revalidate: 10, // In seconds
-//     }
-// }
-
 
 // export async function getStaticPaths() {
 //     const idList = getproducts().map(product => product.id)
